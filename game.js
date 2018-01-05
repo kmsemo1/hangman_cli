f21acb05403d17cd6328a2aa99e9c2fb32f3a60a
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-01-04 20:15:02 -0800

    added main.js

--- game.js
+++ game.js
@@ -3,3 +3,39 @@
 // using node for wordGame.js and inquirer
 var MovieDB = require('./wordGame');
 var inquirer = require('inquirer');
+
+//THIS FUNCTION TAKES IN THE USERPROMPT WHICH ASKS THEM TO TYPE IN THEIR FAVORITE ACTOR 
+function userPrompt(ready) {
+  inquirer.prompt([{
+    //THIS IS MY PROMPT WHICH WILL BE THE FIRST THIS THE USER SEES UPON INITIATING THE GAME. 
+    type: 'confirm',
+    message: 'You ready to play hangman?',
+    name: 'playGame'
+  }]).then(function (inquirerResponse) {
+    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+    if (inquirerResponse.confirm) {
+      console.log("Press any key to guess a letter\n");
+      ready();
+    }
+    else {
+      console.log("\nCome back and play when you're ready.\n");
+    }
+  });
+}
+
+
+// function to pull random word from wordGame.js
+function play(ready) {
+  var randomWord = Math.floor(Math.random() * wordArr.length);
+  randomNumber -= 1;
+  var chosenWord = wordArr[randomNumber];
+  module.exports.chosenWord = chosenWord;
+  ready();
+};
+
+//EXPORTS THE USERPROMPT FOR THE OTHER FILES TO USE. 
+module.exports = {
+  userPrompt
+};
+
+
--- wordGame.js
+++ wordGame.js
@@ -1,6 +1,6 @@
 // array of movie titles used for the game
-var gameWord = ['Bright', 'Star Wars The Last Jedi', 'Jumanji Welcome to the Jungle', 'The Greatest Showman', 'Justice League', 'Blade Runner 2049','The Shape of Water', 'Pitch Perfect 3', 'Dunkirk', 'It', 'Coco', 'Thor Ragnarok', 'Downsizing', "Molly's Game", 'Kingsman: The Golden Circle', 'All the Money in the World', 'Call Me by Your Name', 'Lady Bird', 'Darkest Hour', 'Wonder', 'Ferdinand','The Disaster Artist', 'Mother', 'Guardians of the Galaxy Vol. 2', 'Three Billboards Outside Ebbing, Missouri'];
+var wordArr = ['Bright', 'Star Wars The Last Jedi', 'Jumanji Welcome to the Jungle', 'The Greatest Showman', 'Justice League', 'Blade Runner 2049','The Shape of Water', 'Pitch Perfect 3', 'Dunkirk', 'It', 'Coco', 'Thor Ragnarok', 'Downsizing', "Molly's Game", 'Kingsman: The Golden Circle', 'All the Money in the World', 'Call Me by Your Name', 'Lady Bird', 'Darkest Hour', 'Wonder', 'Ferdinand','The Disaster Artist', 'Mother', 'Guardians of the Galaxy Vol. 2', 'Three Billboards Outside Ebbing, Missouri'];
 
 module.exports = {
-    gameWord
+    wordArr
 };
